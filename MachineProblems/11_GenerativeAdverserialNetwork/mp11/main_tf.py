b9e6fae5c08686f8df5a4a335a0223398da48981
namanuiuc <namanatulshukla@gmail.com>
2018-04-26 00:08:09 -0500

    updated

--- MachineProblems/11_GenerativeAdverserialNetwork/mp11/main_tf.py
+++ MachineProblems/11_GenerativeAdverserialNetwork/mp11/main_tf.py
@@ -6,10 +6,27 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from models.gan import Gan
+import time
 
 
-def train(model, mnist_dataset, learning_rate=0.0005, batch_size=16,
-          num_steps=5000):
+def image_save(model, path='GAN.png'):
+        # Plot out latent space, for +/- 3 std.
+    std = 1
+    x_z = np.linspace(-3 * std, 3 * std, 20)
+    y_z = np.linspace(-3 * std, 3 * std, 20)
+
+    out = np.empty((28 * 20, 28 * 20))
+    for x_idx, x in enumerate(x_z):
+        for y_idx, y in enumerate(y_z):
+            z_mu = np.random.uniform(-1, 1, [16, 10])
+            img = model.generate_samples(z_mu)
+            out[x_idx * 28:(x_idx + 1) * 28,
+                y_idx * 28:(y_idx + 1) * 28] = img[0].reshape(28, 28)
+    plt.imsave(path, out, cmap="gray")
+
+
+def train(model, mnist_dataset, learning_rate=0.001, batch_size=16,
+          num_steps=50000):
     """Implements the training loop of stochastic gradient descent.
 
     Performs stochastic gradient descent with the indicated batch_size and
@@ -22,10 +39,46 @@ def train(model, mnist_dataset, learning_rate=0.0005, batch_size=16,
         batch_size(int): batch size used for training.
         num_steps(int): Number of steps to run the update ops.
     """
-    for step in range(0, num_steps):
+    # Iterations for generator
+    g_iters = 1
+
+    # Iterations for discriminator
+    d_iters = 2
+
+    print('Batch Size: %d, Total epoch: %d, Learning Rate : %f' %
+          (batch_size, num_steps, learning_rate))
+
+    print('Start training ...')
+    tic = time.time()
+    for epoch in range(0, num_steps):
+
         batch_x, _ = mnist_dataset.train.next_batch(batch_size)
-        batch_z = np.random.uniform(-1,1,[batch_size,2])
+        batch_z = np.random.uniform(-1, 1, [batch_size, 10])
         # Train generator and discriminator
+
+        for train_discriminator in range(d_iters):
+            _, d_loss = model.session.run(
+                [model.d_optimizer, model.d_loss],
+                feed_dict={model.x_placeholder: batch_x,
+                           model.z_placeholder: batch_z,
+                           model.learning_rate_placeholder: learning_rate}
+            )
+
+        batch_z = np.random.uniform(-1, 1, [batch_size, 10])
+        for train_generator in range(g_iters):
+            _, g_loss = model.session.run(
+                [model.g_optimizer, model.g_loss],
+                feed_dict={model.z_placeholder: batch_z,
+                           model.learning_rate_placeholder: learning_rate}
+            )
+        if (epoch + 1) % 500 == 0:
+            toc = time.time() - tic
+            tic = time.time()
+            print("%d training steps completed out of %d in %d sec" % (epoch + 1, num_steps, toc))
+            print("g_loss :", g_loss)
+            print("d_loss :", d_loss)
+            print('-' * 50)
+            image_save(model, './output/' + str(epoch + 1))
 
 
 def main(_):
@@ -37,7 +90,7 @@ def main(_):
     mnist_dataset = input_data.read_data_sets('MNIST_data', one_hot=True)
 
     # Build model.
-    model = Gan()
+    model = Gan(nlatent=10)
 
     # Start training
     train(model, mnist_dataset)
--- MachineProblems/11_GenerativeAdverserialNetwork/mp11/models/gan.py
+++ MachineProblems/11_GenerativeAdverserialNetwork/mp11/models/gan.py
@@ -6,9 +6,11 @@ import tensorflow as tf
 from tensorflow import contrib
 from tensorflow.contrib import layers
 
+
 class Gan(object):
     """Adversary based generator network.
     """
+
     def __init__(self, ndims=784, nlatent=2):
         """Initializes a GAN
 
@@ -37,12 +39,24 @@ class Gan(object):
         # Generator loss
         self.g_loss = self._generator_loss(y_hat)
 
+        # Learning rates
+        self.learning_rate_placeholder = tf.placeholder(tf.float32)
+        # self.g_learning_rate_placeholder = tf.placeholder(tf.float32)
+
         # Add optimizers for appropriate variables
+        d_var = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "discriminator")
+        self.d_optimizer = tf.train.AdamOptimizer(
+            learning_rate=self.learning_rate_placeholder,
+            name='d_optimizer').minimize(self.d_loss, var_list=d_var)
+
+        g_var = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "generator")
+        self.g_optimizer = tf.train.AdamOptimizer(
+            learning_rate=self.learning_rate_placeholder,
+            name='g_optimizer').minimize(self.g_loss, var_list=g_var)
 
         # Create session
         self.session = tf.InteractiveSession()
         self.session.run(tf.global_variables_initializer())
-
 
     def _discriminator(self, x, reuse=False):
         """Discriminator block of the network.
@@ -52,14 +66,19 @@ class Gan(object):
             reuse (Boolean): re use variables with same name in scope instead of creating
               new ones, check Tensorflow documentation
         Returns:
-            y (tf.Tensor): Scalar output prediction D(x) for true vs fake image(None, 1). 
+            y (tf.Tensor): Scalar output prediction D(x) for true vs fake image(None, 1).
               DO NOT USE AN ACTIVATION FUNCTION AT THE OUTPUT LAYER HERE.
 
         """
         with tf.variable_scope("discriminator", reuse=reuse) as scope:
-            y = None
-            return y
 
+            # Input
+            hidden_1 = tf.layers.dense(
+                inputs=x, units=512, activation=tf.nn.relu, reuse=reuse)
+
+            y = tf.layers.dense(
+                inputs=hidden_1, units=1, activation=None)
+            return y
 
     def _discriminator_loss(self, y, y_hat):
         """Loss for the discriminator.
@@ -71,9 +90,11 @@ class Gan(object):
             l (tf.Scalar): average batch loss for the discriminator.
 
         """
-        l = 0
+        gt_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(y), logits=y, name="d_loss_gt")
+        gen_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(y_hat), logits=y_hat, name="d_loss_gen")
+        total_loss = gt_loss + gen_loss
+        l = tf.reduce_mean(total_loss)
         return l
-
 
     def _generator(self, z, reuse=False):
         """From a sampled z, generate an image.
@@ -81,14 +102,19 @@ class Gan(object):
         Args:
             z(tf.Tensor): z from _sample_z of dimension (None, 2).
             reuse (Boolean): re use variables with same name in scope instead of creating
-              new ones, check Tensorflow documentation 
+              new ones, check Tensorflow documentation
         Returns:
             x_hat(tf.Tensor): Fake image G(z) (None, 784).
         """
         with tf.variable_scope("generator", reuse=reuse) as scope:
-            x_hat = None
-            return x_hat
 
+            # Input layer
+            hidden_1 = tf.layers.dense(
+                inputs=z, units=64, activation=tf.nn.relu, name='inputs-layer', reuse=reuse)
+
+            x_hat = tf.layers.dense(
+                inputs=hidden_1, units=self._ndims, activation=tf.nn.sigmoid)
+            return x_hat
 
     def _generator_loss(self, y_hat):
         """Loss for the discriminator.
@@ -99,5 +125,20 @@ class Gan(object):
             l (tf.Scalar): average batch loss for the discriminator.
 
         """
-        l = 0
+        # l = tf.nn.sigmoid(-tf.reduce_mean(tf.log(y_hat)))
+        l = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(y_hat), logits=y_hat))
         return l
+
+    def generate_samples(self, z_np):
+        """Generates random samples from the provided z_np.
+
+        Args:
+            z_np(numpy.ndarray): Numpy array of dimension
+                (batch_size, _nlatent).
+
+        Returns:
+            out(numpy.ndarray): The sampled images (numpy.ndarray) of
+                dimension (batch_size, _ndims).
+        """
+        out = self.x_hat.eval(session=self.session, feed_dict={self.z_placeholder: z_np})
+        return out
